585a05d1a8098a87667e1759da350b3f2a1e7ce4
ErnestasAbromavičius <test@mail.com>
2022-12-22 00:19:06 +0200

    database-contacts-1st-attempt

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,33 +3,57 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Classes\Movies;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Kontaktai;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function homepage()
+    function homepage(ManagerRegistry $doctrine)
     {
-        return $this->render('index.html.twig');
 
+        $request = Request::createFromGlobals();
+
+        if ($request->isMethod('POST')) {
+
+            $clientName = $request->request->get('client-name');
+            $clientEmail = $request->request->get('client-email');
+            $message = $request->request->get('client-message');
+            var_dump($clientName);
+            var_dump($clientEmail);
+            var_dump($message);
+            $contact = new Kontaktai();
+
+            $contact->setEmail($clientEmail);
+            $contact->setName($clientName);
+            $contact->setMessage($message);
+
+            $manager = $doctrine->getManager();
+
+            $manager->persist($contact); // pridedam $contact objektą į sąrašą queriu kuriuos mes vykdysim
+
+            $manager->flush(); // mes įvykdom visus querius.
+
+        }
+        return $this->render('index.html.twig');
     }
 
     #[Route('/about-us', name: 'about-us')]
-    public function aboutUs()
+    function aboutUs()
     {
         return $this->render('aboutUs.html.twig');
     }
 
     #[Route('/services', name: 'services')]
-    public function services()
+    function services()
     {
         return $this->render('services.html.twig');
     }
 
     #[Route('/careers', name: 'careers')]
-    public function careers()
+    function careers()
     {
         return $this->render('careers.html.twig');
     }
